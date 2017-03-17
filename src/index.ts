0445e9ee09f5beb6a3b1a635d1f1e36f94856cf0
Simen Vie <simenvie@gmail.com>
2017-03-17 13:34:29 +0100

    added mysql connection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
 import app from './app';
 
-app.listen(8080, () => {
-	console.log('faak');
+import * as mysql from 'mysql';
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'simo',
+  password : 'simo',
+  database : 'studentlyfedb'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error(err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+let port = 8080;
+app.listen(port, () => {
+	console.log('Server is listening on port: ' + port);
 });
