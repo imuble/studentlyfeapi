cb7084362a37e7f609afb383888a545c856e7edb
Simen Vie <simenvie@gmail.com>
2017-03-17 14:50:43 +0100

    added repo func

--- src/app/user/repository.ts
+++ src/app/user/repository.ts
@@ -1,18 +1,38 @@
-import { UserModel } from './model';
+import { IUser, UserSchema } from './model';
 
 export class UserRepository {
 	constructor() {
 	}
 
-	public findById (id: string): UserModel {
-		const user: UserModel = {
-			id: id,
-			username: 'Simen',
-			fbId: 'wtf'
-		};
-
-		return user;
+	/**
+	 * Finds a specified user
+	 * @param {string} id - id of the user
+	 * @param {Function} completion - Function that will execute after the query, called completion(err, user)
+	 */
+	public findById(id: string, completion: Function): void {
+		UserSchema.findById(id).exec()
+			.then((user) => {
+				completion(null, user);
+			})
+			.catch((err) => {
+				completion(err);
+			});
 	}
-	public create (user: UserModel): void {
+
+
+	/**
+	 * Finds a specified user
+	 * @param {IUser} user - user object to save
+	 * @param {Function} completion - Function that will execute after the query, called completion(err, user)
+	 */
+	public create(user: IUser, completion: Function): void {
+		let newUser = new UserSchema(user);
+		newUser.save().exec()
+			.then((savedUser) => {
+				completion();
+			})
+			.catch((err) => {
+				completion(err);
+			});
 	}
 }
