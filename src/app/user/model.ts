e666410df71eb49869d91ee52ca94417b2cda51c
lorang92 <Steffen.ekeberg@gmail.com>
2017-03-17 14:42:03 +0100

    added user model

--- src/app/user/model.ts
+++ src/app/user/model.ts
@@ -1,5 +1,13 @@
-export interface UserModel {
-	id: string;
-	username: string;
-	fbId: string;
-}
+var mongoose = require('mongoose');
+
+export interface IUser extends mongoose.Document {
+	name: string,
+	fbId: string
+};
+
+export const UserSchema = new mongoose.Schema({
+	name: {type:String, required: true},
+	fbId: {type:String, required: true},
+});
+
+const User = mongoose.model<IUser>('User', UserSchema);
