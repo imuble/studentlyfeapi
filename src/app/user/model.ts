328376cd466813db801a32f1797f4709a2f55ee5
lorang92 <Steffen.ekeberg@gmail.com>
2017-03-17 15:39:09 +0100

    Added attributes to the user object

--- src/app/user/model.ts
+++ src/app/user/model.ts
@@ -1,13 +1,16 @@
 import mongoose = require('mongoose');
+import AttributeSchema from "../attribute/model";
 
 export interface IUser extends mongoose.Document {
 	name: string,
-	fbId: string
+	fbId: string,
+	attributes?: [{key: mongoose.Schema.Types.ObjectId, value: number}],
 };
 
 export const UserSchema = new mongoose.Schema({
 	name: {type:String, required: true},
 	fbId: {type:String, required: true},
+	attributes: [{key: mongoose.Schema.Types.ObjectId, value: Number}]
 });
 
 const User = mongoose.model<IUser>('User', UserSchema)
