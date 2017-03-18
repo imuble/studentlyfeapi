b7bc3fe6a7cbd9e9dc81682c910b94fd93d8fd1f
Simen Vie <simenvie@gmail.com>
2017-03-18 12:03:54 +0100

    set isAdmin to optional

--- src/app/user/model.ts
+++ src/app/user/model.ts
@@ -4,7 +4,7 @@ import AttributeSchema from "../attribute/model";
 export interface IUser extends mongoose.Document {
 	name?: string,
 	fbId: string,
-	isAdmin: Boolean,
+	isAdmin?: Boolean,
 	attributes?: [{key: {type: mongoose.Schema.Types.ObjectId}, value: number}],
 };
 
