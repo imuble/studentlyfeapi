069fc304179d84a0d0dbf240fee09da56897b078
Simen Vie <simenvie@gmail.com>
2017-03-18 12:02:03 +0100

    added isAdmin to user

--- src/app/user/model.ts
+++ src/app/user/model.ts
@@ -4,12 +4,14 @@ import AttributeSchema from "../attribute/model";
 export interface IUser extends mongoose.Document {
 	name?: string,
 	fbId: string,
+	isAdmin: Boolean,
 	attributes?: [{key: {type: mongoose.Schema.Types.ObjectId}, value: number}],
 };
 
 export const UserSchema = new mongoose.Schema({
 	name: {type:String},
 	fbId: {type:String, required: true},
+	isAdmin: {type:Boolean, default:false},
 	attributes: [{key: {type: mongoose.Schema.Types.ObjectId, ref: 'Attribute'}, value: Number}]
 });
 
