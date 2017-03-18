9702f989e4b755db329c5c63b82b08871bb10534
lorang92 <Steffen.ekeberg@gmail.com>
2017-03-18 15:52:21 +0100

    added image url field in activity model

--- src/app/activity/model.ts
+++ src/app/activity/model.ts
@@ -4,14 +4,16 @@ export interface IActivity extends mongoose.Document {
     name: string,
     selfEffects?: [{key: {type:  string}}],
     targetEffects?: [{key: {type: string}}],
-    successFactors?: [{key: {type: string}, value: Number}]
+    successFactors?: [{key: {type: string}, value: Number}],
+    image?: string,
 };
 
 export const ActivitySchema = new mongoose.Schema({
     name: {type:String, required: true},
     selfEffects: [{key: {type: mongoose.Schema.Types.ObjectId, ref: 'ActivityEffect'}}],
     targetEffects: [{key: {type: mongoose.Schema.Types.ObjectId, ref: 'ActivityEffect'}}],
-    successFactors: [{key: {type: mongoose.Schema.Types.ObjectId, ref: 'Attribute'}, value: Number}]
+    successFactors: [{key: {type: mongoose.Schema.Types.ObjectId, ref: 'Attribute'}, value: Number}],
+    image: string,
 });
 
 
