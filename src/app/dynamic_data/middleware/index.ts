621bc5cbc8fe2277417273798c18e472f6a26537
lorang92 <Steffen.ekeberg@gmail.com>
2017-04-01 11:26:33 +0200

    typo

--- src/app/dynamic_data/middleware/index.ts
+++ src/app/dynamic_data/middleware/index.ts
@@ -21,7 +21,7 @@ export function returnSuccessResponseWithDynamicData(req, res, next) {
 
 export function findUserObject(req, res, next) {
     let userId = req.data.decodedToken.userId;
-    UserRepository.findByIdAndPopulate(userId, "attribute", (err, user) => {
+    UserRepository.findByIdAndPopulate(userId, "attributes", (err, user) => {
         if (err) return res.status(500).send();
         if (!user) return res.status(401).send();
         req.data.user = user;
