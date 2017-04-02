558f3a4eec458149c561594a78d7e5b445165175
Simen Vie <simenvie@gmail.com>
2017-04-02 12:21:03 +0200

    removed activity effects component, it is also inside user component

--- src/app/activity_effect/index.ts
+++
@@ -1,9 +0,0 @@
-import ActivityEffectRepository from './repository';
-import ActivityEffectRoute from './route'
-
-const ActivityEffectFolder = {
-    route: ActivityEffectRoute,
-    repository: ActivityEffectRepository
-};
-
-export default ActivityEffectFolder;
