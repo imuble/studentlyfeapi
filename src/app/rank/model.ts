9390246a7bb88870a2699485ed86ae36b77ebd26
Petter Iversen <petteriversen@dhcp-10-22-39-52.wlan.ntnu.no>
2017-04-03 19:01:31 +0200

    rename property imgUrl to imageUrl

--- src/app/rank/model.ts
+++ src/app/rank/model.ts
@@ -3,13 +3,13 @@ import mongoose = require('mongoose');
 export interface IRank extends mongoose.Document {
     name: string,
     requiredExperience: Number
-    imgUrl?: string
+    imageUrl?: string
 };
 
 export const RankSchema = new mongoose.Schema({
     name: {type:String, required: true},
     requiredExperience: {type: Number, required: true},
-    imgUrl: {type: String}
+    imageUrl: {type: String}
 });
 
 const Rank = mongoose.model<IRank>('Rank', RankSchema);
